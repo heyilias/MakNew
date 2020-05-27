c706165a57bec0906367830244cd22d053b17d70
unknown <ilias7576@gmail.com>
2020-05-27 21:21:46 +0000

    new things

---
+++ ProjectMaleabAlKorbV2/Controllers/ReservationController.cs
@@ -0,0 +1,31 @@
+﻿using ProjectMaleabAlKorbV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectMaleabAlKorbV2.Controllers
+{
+    public class ReservationController : Controller
+    {
+        MalaebAlKorbEntities db = new MalaebAlKorbEntities();
+        // GET: Reservation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Reservation reservation)
+        {
+            reservation.dateReservation = DateTime.Now;
+            reservation.Email = Session["emails"].ToString();
+            db.Reservations.Add(reservation);
+            db.SaveChanges();
+            return View();
+        }
+
+
+    }
+}
